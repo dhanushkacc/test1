707485b8040c22f2c910e55568b9f40fd034bcf2
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2025-01-06 22:09:26 +0530

    sds

--- test2.js
+++ test2.js
@@ -2,3 +2,8 @@ const request = require('supertest');
 const express = require('express');
 const app = require('../index');
 
+beforeEach(() => {
+    products = [
+        { id: 1, name: 'Laptop', price: 1000.0 },
+        { id: 2, name: 'Smartphone', price: 800.0 }
+    ];
