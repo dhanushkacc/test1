68ce7dcbd76002686a59df6a41d631fc9c044ccf
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2025-01-06 22:11:50 +0530

    asas

--- test2.js
+++ test2.js
@@ -7,3 +7,12 @@ beforeEach(() => {
         { id: 1, name: 'Laptop', price: 1000.0 },
         { id: 2, name: 'Smartphone', price: 800.0 }
     ];
+
+
+
+
+    test('GET /api/products should return product list', async () => {
+        const res = await request(app).get('/api/products');
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toEqual(products);
+    });
