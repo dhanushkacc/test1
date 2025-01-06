9f8c037a9ad120264b4b95908fd7922c6a1468a2
dhanushkacc <136559309+dhanushkacc@users.noreply.github.com>
2025-01-06 22:10:42 +0530

    asas

--- test2.js
+++ test2.js
@@ -5,5 +5,5 @@ const app = require('../index');
 beforeEach(() => {
     products = [
         { id: 1, name: 'Laptop', price: 1000.0 },
-        
+        { id: 2, name: 'Smartphone', price: 800.0 }
     ];
